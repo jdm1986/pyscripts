0a94f1fc179569ce0c014bfdd84a92b42eb8870c
Josh Mix <joshuadmix@gmail.com>
2018-02-20 20:40:46 -0500

    added column name change to allow for previously duplicate job + company address, city, state column names to be included

--- CDC_cleanup.py
+++ CDC_cleanup.py
@@ -4,8 +4,16 @@ import pandas as pd
 import datetime
 
 c = pd.read_csv("CDC.csv")
+
+c.rename(columns={c.columns[25]: ''}).inplace = True
+c.rename(columns={c.columns[27]: ''}).inplace = True
+c.rename(columns={c.columns[28]: ''}).inplace = True
+
+# c.to_csv("cdc_test.csv", index=False)
+
 clean_col = ['Project Name', 'Bid Date', 'Estimated Start Date', 'Type', 'Status', 'Value', 'Address', 'City', 'State',
-             'Company Name', 'Role', 'Web Address', 'Project Construction Data Company URL']
+             'Company Name', 'Address.1', 'City.1', 'State.1', 'Role', 'Web Address',
+             'Project Construction Data Company URL']
 cleaned = c[clean_col]
 
 cleaned2 = (cleaned.loc[cleaned['Role'].isin(['Owner', 'General Contractor', 'Apparent Low General Contractor',
