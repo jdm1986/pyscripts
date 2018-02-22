58d6ffc63662a5424eca5470f41164c1b26f795b
Josh Mix <joshuadmix@gmail.com>
2018-02-22 08:12:45 -0500

    Increased job filtering based on 'Type' column.

--- CDC_cleanup.py
+++ CDC_cleanup.py
@@ -16,10 +16,12 @@ clean_col = ['Project Name', 'Bid Date', 'Estimated Start Date', 'Type', 'Status
              'Project Construction Data Company URL']
 cleaned = c[clean_col]
 
-cleaned2 = (cleaned.loc[cleaned['Role'].isin(['Owner', 'General Contractor', 'Apparent Low General Contractor',
+cleaned = (cleaned.loc[cleaned['Role'].isin(['Owner', 'General Contractor', 'Apparent Low General Contractor',
                                               'Awarded General Contractor', 'Construction Manager'])])
 
-cleaned3 = cleaned2.sort_values(cleaned2.columns[0])
+cleaned = (cleaned.loc[~cleaned['Type'].isin(['Renovation', 'Roof Replacement', 'Remodeling'])])
+
+cleaned = cleaned.sort_values(cleaned.columns[0])
 
 now = datetime.datetime.now()
-cleaned3.to_csv(f"CDC {now.month}-{now.day}-{now.year}.csv", index=False)
+cleaned.to_csv(f"CDC {now.month}-{now.day}-{now.year}.csv", index=False)
